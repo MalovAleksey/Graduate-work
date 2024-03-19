28f1df18a6020d83b12b65438a072f12040b6ed5
Aleksey <tandem423@rambler.ru>
2024-03-19 13:45:35 +0300

    2-vm-and-target_group

--- terraform/main.tf
+++ terraform/main.tf
@@ -110,3 +110,19 @@ resource "yandex_vpc_network" "network-1" {
   network_id = "${yandex_vpc_network.network-1.id}"
 }
 
+
+
+resource "yandex_alb_target_group" "foo" {
+    name         = "homework"
+
+  target {
+    subnet_id    = "${yandex_vpc_subnet.subnet-1.id}"
+    ip_address   = "${yandex_compute_instance.vm-1.network_interface.0.ip_address}"
+  }
+
+  target {
+    subnet_id    = "${yandex_vpc_subnet.subnet-2.id}"
+    ip_address   = "${yandex_compute_instance.vm-2.network_interface.0.ip_address}"
+  }
+  
+}
--- terraform/output.tf
+++ terraform/output.tf
@@ -1,7 +1,24 @@
-output "ip_adress_vm1"{
+output "nat_ip_adress_vm1"{
     value = yandex_compute_instance.vm-1.network_interface.0.nat_ip_address
 }
 
-output "ip_adress_vm2"{
+output "nat_ip_adress_vm2"{
     value = yandex_compute_instance.vm-2.network_interface.0.nat_ip_address
+}
+
+
+output "ip_adress_vm1"{
+    value = yandex_compute_instance.vm-1.network_interface.0.ip_address
+}
+
+output "ip_adress_vm2"{
+    value = yandex_compute_instance.vm-2.network_interface.0.ip_address
+}
+
+output "subnet_id_vm1"{
+    value = yandex_vpc_subnet.subnet-1.id
+}
+
+output "subnet_id_vm2"{
+    value = yandex_vpc_subnet.subnet-2.id
 }
