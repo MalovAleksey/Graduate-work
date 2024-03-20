ef69d20629b4c517dcab1a4358c4135398a5931c
Aleksey <tandem423@rambler.ru>
2024-03-20 09:28:19 +0300

    3-vm-and-target_group

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,13 +16,22 @@ resource "yandex_compute_disk" "boot-disk-1" {
   image_id = "fd88r89var8ukrlbmaki"
 } 
 
+resource "yandex_compute_disk" "boot-disk-2" {
+  name     = "sda-2"
+  type     = "network-hdd"
+  zone     = var.zona-3
+  size     = "15"
+  image_id = "fd88r89var8ukrlbmaki"
+} 
+
 #####################################################################
 
-resource "yandex_compute_instance" "vm-1" {
+resource "yandex_compute_instance" "nginx-1" {
   name                      = "linux-vm-1"
   allow_stopping_for_update = true
   platform_id               = "standard-v2"
   zone                      = var.zona-1
+  #user_data                 = file(var.skript_enginx)
 
  resources {
 
@@ -54,11 +63,12 @@ resource "yandex_compute_instance" "vm-1" {
 ###########################################################################
 
  
-resource "yandex_compute_instance" "vm-2" {
-  name                      = "linux-vm-2"
+resource "yandex_compute_instance" "nginx-2" {
+    name                      = "linux-vm-2"
   allow_stopping_for_update = true
   platform_id               = "standard-v2"
   zone                      = var.zona-2
+  #user_data                 = file(var.skript_enginx)
 
  resources {
 
@@ -91,6 +101,40 @@ resource "yandex_compute_instance" "vm-2" {
 
 ############################################################################################
 
+resource "yandex_compute_instance" "zabbix" {
+    name                      = "linux-vm-3"
+  allow_stopping_for_update = true
+  platform_id               = "standard-v2"
+  zone                      = var.zona-3
+  
+
+ resources {
+
+    core_fraction = 5
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    disk_id = yandex_compute_disk.boot-disk-2.id
+  }
+
+  network_interface {
+    subnet_id = "${yandex_vpc_subnet.subnet-3.id}"
+    nat       = true
+  }
+
+  metadata = {
+    #ssh-keys = ""
+  user-data = file(var.yaml)
+  }
+
+  scheduling_policy {
+    preemptible = true
+  }
+}
+
+#############################################################################################
 
 resource "yandex_vpc_network" "network-1" {
   name = "network1"
@@ -110,19 +154,28 @@ resource "yandex_vpc_network" "network-1" {
   network_id = "${yandex_vpc_network.network-1.id}"
 }
 
+resource "yandex_vpc_subnet" "subnet-3" {
+  name           = "subnet3"
+  zone           = var.zona-3
+  v4_cidr_blocks = ["192.168.12.0/24"]
+  network_id = "${yandex_vpc_network.network-1.id}"
+}
 
+#######################################################################################################
 
 resource "yandex_alb_target_group" "foo" {
     name         = "homework"
 
   target {
     subnet_id    = "${yandex_vpc_subnet.subnet-1.id}"
-    ip_address   = "${yandex_compute_instance.vm-1.network_interface.0.ip_address}"
+    ip_address   = "${yandex_compute_instance.nginx-1.network_interface.0.ip_address}"
   }
 
   target {
     subnet_id    = "${yandex_vpc_subnet.subnet-2.id}"
-    ip_address   = "${yandex_compute_instance.vm-2.network_interface.0.ip_address}"
+    ip_address   = "${yandex_compute_instance.nginx-2.network_interface.0.ip_address}"
   }
   
 }
+
+#######################################################################################################
--- terraform/output.tf
+++ terraform/output.tf
@@ -1,24 +1,39 @@
-output "nat_ip_adress_vm1"{
-    value = yandex_compute_instance.vm-1.network_interface.0.nat_ip_address
+output "nat_ip_adress_nginx-1"{
+    value = yandex_compute_instance.nginx-1.network_interface.0.nat_ip_address
 }
 
-output "nat_ip_adress_vm2"{
-    value = yandex_compute_instance.vm-2.network_interface.0.nat_ip_address
+output "nat_ip_adress_nginx-2"{
+    value = yandex_compute_instance.nginx-2.network_interface.0.nat_ip_address
 }
 
-
-output "ip_adress_vm1"{
-    value = yandex_compute_instance.vm-1.network_interface.0.ip_address
+output "nat_ip_adress_zabbix"{
+    value = yandex_compute_instance.zabbix.network_interface.0.nat_ip_address
 }
 
-output "ip_adress_vm2"{
-    value = yandex_compute_instance.vm-2.network_interface.0.ip_address
+#########################################################################################
+
+output "ip_adress_nginx-1"{
+    value = yandex_compute_instance.nginx-1.network_interface.0.ip_address
 }
 
-output "subnet_id_vm1"{
+output "ip_adress_nginx-2"{
+    value = yandex_compute_instance.nginx-2.network_interface.0.ip_address
+}
+
+output "ip_adress_zabbix"{
+    value = yandex_compute_instance.zabbix.network_interface.0.ip_address
+}
+
+###########################################################################################
+
+output "subnet_id_nginx-1"{
     value = yandex_vpc_subnet.subnet-1.id
 }
 
-output "subnet_id_vm2"{
+output "subnet_id_nginx-2"{
     value = yandex_vpc_subnet.subnet-2.id
+}
+
+output "subnet_id_zabbix"{
+    value = yandex_vpc_subnet.subnet-3.id
 }
