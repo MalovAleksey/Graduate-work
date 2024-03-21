b3232976f99a319824f1176f75c48272745bbeac
Aleksey <tandem423@rambler.ru>
2024-03-21 11:27:36 +0300

    5-vm-AND-target_group-AND-security_group-AND-backend_group-AND-http_router

--- terraform/main.tf
+++ terraform/main.tf
@@ -334,3 +334,27 @@ resource "yandex_alb_backend_group" "test-backend-group" {
     http2 = "true"
   }
 }
+
+##############################################################################################
+
+resource "yandex_alb_http_router" "tf-router" {
+  name          = "tf-router"
+  labels        = {
+    tf-label    = "tf-label-value"
+    empty-label = ""
+  }
+}
+
+resource "yandex_alb_virtual_host" "my-virtual-host" {
+  name                    = "vhost"
+  http_router_id          = yandex_alb_http_router.tf-router.id
+  route {
+    name                  = "my-route"
+    http_route {
+      http_route_action {
+        backend_group_id  = yandex_alb_backend_group.test-backend-group.id
+        timeout           = "60s"
+      }
+    }
+  }
+}    
